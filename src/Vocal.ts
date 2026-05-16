1e70c52353d09adc799b5ab3d04a582c2e7548a4
untemps <v.lebadezet@untemps.net>
2026-05-16 18:23:19 +0200

    feat(vocal): Add once() method for one-shot event listener registration

--- src/Vocal.ts
+++ src/Vocal.ts
@@ -217,6 +217,18 @@ class Vocal {
 		return this
 	}
 
+	once<T extends EventType>(eventType: T, callback: EventHandlerFor<T>): this
+	once(eventType: string, callback: EventHandler): this {
+		if (!this._includesEventType(eventType)) {
+			throw new Error(this._unknownEventTypeMessage(eventType))
+		}
+		const wrapper: EventHandler = (...args) => {
+			;(callback as EventHandler).call(this, ...args)
+			this.removeEventListener(eventType as EventType, wrapper as EventHandlerFor<EventType>)
+		}
+		return this.addEventListener(eventType as EventType, wrapper as EventHandlerFor<EventType>)
+	}
+
 	cleanup(): this {
 		this.stop()
 
